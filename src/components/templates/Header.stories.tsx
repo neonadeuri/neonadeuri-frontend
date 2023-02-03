4b5cb3048ad688c4a84065749484db7761edc321
nosicknnodie <nosicknnodie@naver.com>
2023-02-03 22:52:26 +0900

    Header Storybook 추가

---
+++ src/components/templates/Header.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Header from './Header';
+
+export default {
+  title: 'atoms/Header',
+  component: Header,
+  // argTypes: {
+  //   size: {
+  //     options: ['sm', 'xs', 'lg', 'base', 'xl', '2xl'],
+  //     control: { type: 'select' },
+  //   },
+  // },
+} as ComponentMeta<typeof Header>;
+
+const Template: ComponentStory<typeof Header> = (args) => <Header />;
+
+export const LoadingTemp = Template.bind({});
