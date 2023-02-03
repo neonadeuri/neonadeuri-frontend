cc34e0a752806ba42f1cc83c24d1ee824a33b3b2
nosicknnodie <nosicknnodie@naver.com>
2023-02-03 17:54:20 +0900

    Loading storybook control 추가

--- src/components/atoms/Loading.stories.tsx
+++ src/components/atoms/Loading.stories.tsx
@@ -6,8 +6,16 @@ import Loading from './Loading';
 export default {
   title: 'atoms/Loading',
   component: Loading,
+  argTypes: {
+    size: {
+      options: ['sm', 'xs', 'lg', 'base', 'xl', '2xl'],
+      control: { type: 'select' },
+    },
+  },
 } as ComponentMeta<typeof Loading>;
 
-const Template: ComponentStory<typeof Loading> = () => <Loading />;
+const Template: ComponentStory<typeof Loading> = (args) => (
+  <Loading {...args} />
+);
 
 export const LoadingTemp = Template.bind({});
