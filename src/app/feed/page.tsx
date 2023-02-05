a37f6f1f18d0d7328654d30f97ab3125aa430ea3
nosicknnodie <nosicknnodie@naver.com>
2023-02-05 18:56:34 +0900

    [#36] 깃헙 이슈 생성시 브랜치 자동생성 Fix (#37)
    * Update README.md (#35)
    
    * Feature/feed box (#33)
    
    * style : tailwind css add
    
    * feat : 피드페이지의 박스 컴포넌트 생성
    
    * feat : storybook feedbox
    
    * feat : 피드 페이지,레이아웃 스타일 변경 및 박스 컴포넌트 추가
    
    * refactor: auto create branch update
    
    ---------
    
    Co-authored-by: ohseil <sioh0612@gmail.com>
    Co-authored-by: leeuihyun <88185154+leeuihyun@users.noreply.github.com>

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import FeedBox from 'components/templates/FeedBox';
 
 export default function Page() {
-  return <div>피드</div>;
+  return (
+    <div className="w-full h-full flex justify-center  items-center flex-col">
+      <FeedBox></FeedBox>
+    </div>
+  );
 }
