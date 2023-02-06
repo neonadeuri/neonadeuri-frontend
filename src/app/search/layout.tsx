1a3f16ad56419ab716e9de2f7625eb247dfa3198
nosicknnodie <nosicknnodie@naver.com>
2023-02-06 17:32:20 +0900

    style: UI 구현

--- src/app/search/layout.tsx
+++ src/app/search/layout.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="h-full w-full">
-      <div className="h-full px-6 py-4">{children}</div>
-    </div>
-  );
+  return <div className="h-full w-full">{children}</div>;
 }
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,12 @@
+import SearchInput from 'components/templates/SearchInput';
 import React from 'react';
 
 export default function Page() {
-  return <div>검색</div>;
+  return (
+    <div className="w-full h-full flex justify-center">
+      <div className="max-w-7xl max-h-20 h-20 w-full flex justify-start pl-2 items-center border-b border-gray-200">
+        <SearchInput />
+      </div>
+    </div>
+  );
 }
