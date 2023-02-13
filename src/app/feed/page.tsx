d4bd83631c38744177555fdeadfc081f0b1e9f4b
leeuihyun <88185154+leeuihyun@users.noreply.github.com>
2023-02-13 14:25:42 +0900

    [Refactor/#44] FeedBox 수정 (#47)
    Co-authored-by: nosicknnodie <nosicknnodie@naver.com>

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -3,8 +3,22 @@ import FeedBox from 'components/organism/FeedBox';
 
 export default function Page() {
   return (
-    <div className="w-full h-full flex justify-center  items-center flex-col">
-      <FeedBox></FeedBox>
+    <div className="w-full h-full flex justify-center items-center">
+      <div className="flex max-w-7xl flex-wrap">
+        <FeedBox
+          nickname="asdfsdfdsa"
+          review="dsafsdafdsafdsfdsafdsafdsfdasdfafdsfdsafdsfdsafdsafsdafsdasa"
+        ></FeedBox>
+        <FeedBox nickname="asdfsdfdsa" review="dsaa"></FeedBox>
+        <FeedBox
+          nickname="asdfsdfdsa"
+          review="dsafsdafdsafdsfdsafdsafdsfdsa"
+        ></FeedBox>
+        <FeedBox
+          nickname="asdfsdfdsa"
+          review="dsafsdafdsafdsfdsafdsafdsfdsa"
+        ></FeedBox>
+      </div>
     </div>
   );
 }
--- src/components/organism/FeedBox.tsx
+++ src/components/organism/FeedBox.tsx
@@ -1,13 +1,19 @@
 'use client';
 
 import Image from 'next/image';
+import UserReview from './UserReview';
 
-const FeedBox = () => {
+export interface Review {
+  nickname: string;
+  review: string;
+}
+
+const FeedBox = (props: Review) => {
   return (
-    <div className="w-9/12 h-80 bg-amber-300 px-4 flex justify-center items-center rounded-md mb-12">
-      <div className="w-full h-full flex justify-around items-center">
-        <div className="flex justify-center h-5/6">
-          <div className="rounded-lg shadow-lg bg-white w-72">
+    <div className="h-80 bg-amber-300 px-6 flex justify-center items-center rounded-md mb-5 max-w-2xl mx-auto">
+      <div className="w-full flex justify-around items-center">
+        <div className="flex justify-center">
+          <div className="rounded-lg shadow-lg bg-white w-52 mr-2">
             <a href="#!">
               <Image
                 className="rounded-t-lg"
@@ -25,26 +31,14 @@ const FeedBox = () => {
             </div>
           </div>
         </div>
-        <div className="flex justify-center flex-col">
-          <div className="block p-4 rounded-lg shadow-lg bg-white max-w-sm mb-1">
-            <h5 className="text-gray-900  text-lg leading-tight font-medium mb-1">
-              nickname
-            </h5>
-            <p className="text-gray-700 text-base overflow-ellipsis overflow-hidden  whitespace-nowrap">
-              한줄평입니다.한줄평입니다.한줄평입니다.한줄평입니다.한줄평입니다.한줄평입니다.
-            </p>
-          </div>
-          <div className="block p-4 rounded-lg shadow-lg bg-white max-w-sm mb-1">
-            <h5 className="text-gray-900  text-lg leading-tight font-medium mb-1">
-              nickname
-            </h5>
-            <p className="text-gray-700 text-base overflow-ellipsis overflow-hidden  whitespace-nowrap">
-              한줄평입니다.한줄평입니다.한줄평입니다.한줄평입니다.한줄평입니다.한줄평입니다.
-            </p>
-          </div>
+        <div className="flex justify-center flex-col max-w-xs  w-80">
+          <UserReview
+            nickname={props.nickname}
+            review={props.review}
+          ></UserReview>
           <button
             type="button"
-            className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
           >
             더 보러가기
           </button>
