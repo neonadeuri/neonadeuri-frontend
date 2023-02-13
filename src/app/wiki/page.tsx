db981149fe91719d1c858fed74d284e23b13241d
leeuihyun <meta212121@naver.com>
2023-02-13 20:09:10 +0900

    feat : 공유도서 페이지 layout, page 생성

---
+++ src/app/wiki/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import BookWikiTemplate from 'components/template/BookWikiTemplate';
+
+export default function Page() {
+  return (
+    <div className="w-full h-full flex justify-center">
+      <BookWikiTemplate />
+    </div>
+  );
+}
